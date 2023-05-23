65233dd528978830ef7fd52169eeb217c5608eaf
Patricio Pagano <patricio.pagano@geosystems.com.ar>
2023-05-23 09:27:05 -0300

    Obteniendo las categorías de la API

--- src/context/CategoriaProvider.jsx
+++ src/context/CategoriaProvider.jsx
@@ -1,10 +1,27 @@
-import { useState, useEffect, useContext } from "react";
+import { useState, useEffect, createContext } from "react";
+import axios from "axios";
 
 const CatgeoriasContext = createContext();
 
 const CategoriasProvider = ({ children }) => {
+  const [categorias, setCategorias] = useState([]);
+  const obtenerCategorias = async () => {
+    try {
+      const url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list";
+      const { data } = await axios.get(url);
+      setCategorias(data.drinks);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    obtenerCategorias();
+    return () => {};
+  }, []);
+
   return (
-    <CatgeoriasContext.Provider value={{}}>
+    <CatgeoriasContext.Provider value={{ categorias }}>
       {children}
     </CatgeoriasContext.Provider>
   );
